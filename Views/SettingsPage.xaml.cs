c8699d47000bc69bc3507c23f6148da2344e153a
Przemysław Przybyszewski <103174443+phoneuooyd@users.noreply.github.com>
2025-07-20 21:36:30 +0200

    Revert "Add language switcher in settings page (#54)"
    This reverts commit f7556d2e1b41619e7eea260e2946143c7962cffc.

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,41 +1,11 @@
 using Microsoft.Maui.Controls;
-using Foodbook.Services;
-using System.Collections.Generic;
-using System.Linq;
-using FoodbookApp;
 
 namespace Foodbook.Views;
 
 public partial class SettingsPage : ContentPage
 {
-    private readonly ILocalizationService _localizationService;
-    private readonly List<(string Culture, string DisplayName)> _languages = new()
-    {
-        ("en-US", "English"),
-        ("pl-PL", "Polski")
-    };
-
-    public SettingsPage(ILocalizationService localizationService)
+    public SettingsPage()
     {
         InitializeComponent();
-        _localizationService = localizationService;
-
-        LanguagePicker.ItemsSource = _languages.Select(l => l.DisplayName).ToList();
-        var index = _languages.FindIndex(l => l.Culture == _localizationService.CurrentCulture.Name);
-        if (index >= 0)
-            LanguagePicker.SelectedIndex = index;
-    }
-
-    private void OnLanguageSelected(object sender, EventArgs e)
-    {
-        if (LanguagePicker.SelectedIndex < 0 || LanguagePicker.SelectedIndex >= _languages.Count)
-            return;
-
-        var culture = _languages[LanguagePicker.SelectedIndex].Culture;
-        if (_localizationService.CurrentCulture.Name == culture)
-            return;
-
-        _localizationService.SetCulture(culture);
-        Application.Current.MainPage = new AppShell();
     }
 }
