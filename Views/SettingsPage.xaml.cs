0aebf91e158fea270c4375cdfadf725bd4ed54d8
Przemysław Przybyszewski <103174443+phoneuooyd@users.noreply.github.com>
2025-07-20 20:32:49 +0200

    Register AppShell and update language refresh

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,11 +1,25 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using FoodbookApp;
 
 namespace Foodbook.Views;
 
 public partial class SettingsPage : ContentPage
 {
-    public SettingsPage()
+    private readonly Foodbook.Services.ILocalizationService _localizationService;
+
+    public SettingsPage(Foodbook.Services.ILocalizationService localizationService)
     {
+        _localizationService = localizationService;
         InitializeComponent();
+    }
+
+    private void OnLanguageSelected(object sender, EventArgs e)
+    {
+        if (sender is Picker picker && picker.SelectedItem is string culture)
+        {
+            _localizationService.SetCulture(culture);
+            Application.Current.MainPage = MauiProgram.ServiceProvider!.GetRequiredService<AppShell>();
+        }
     }
 }
