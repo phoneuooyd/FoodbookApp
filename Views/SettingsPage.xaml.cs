c59d0b66076a215bc6ada6764064e125f79454b3
Przemysław Przybyszewski <103174443+phoneuooyd@users.noreply.github.com>
2025-07-21 21:08:09 +0200

    Add runtime localization service and picker

--- Views/SettingsPage.xaml.cs
+++ Views/SettingsPage.xaml.cs
@@ -1,11 +1,27 @@
 using Microsoft.Maui.Controls;
+using Foodbook.Services.Localization;
+using System.Globalization;
 
 namespace Foodbook.Views;
 
 public partial class SettingsPage : ContentPage
 {
-    public SettingsPage()
+    private readonly LocalizationResourceManager _locManager;
+
+    public SettingsPage(LocalizationResourceManager locManager)
     {
+        _locManager = locManager;
         InitializeComponent();
+
+        LanguagePicker.SelectedItem = _locManager.CurrentCulture.TwoLetterISOLanguageName;
+    }
+
+    private void OnLanguageChanged(object sender, EventArgs e)
+    {
+        if (LanguagePicker.SelectedItem is string code)
+        {
+            string culture = code == "pl" ? "pl-PL" : "en-US";
+            _locManager.SetCulture(culture);
+        }
     }
 }
