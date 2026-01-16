807aa6a8696d73ac40b72b5a837c3582f3281129
Przemysław Przybyszewski <103174443+phoneuooyd@users.noreply.github.com>
2026-01-16 13:43:38 +0100

    migracja
    -

---
+++ FoodbookApp.App/Data/Migrations/20260116124019_AddUpdatedAtColumns.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodbookApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ShoppingListItems",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "ShoppingListItems",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "RecipeLabels",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Plans",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Plans",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "PlannedMeals",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "PlannedMeals",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Ingredients",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Ingredients",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Folders",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "ShoppingListItems");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "ShoppingListItems");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "RecipeLabels");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Plans");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "PlannedMeals");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "PlannedMeals");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Folders");
+        }
+    }
+}
